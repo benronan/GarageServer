8a6a3726c44ff3f55feaee3cc6476e7b9afd3f44
benronan <benronan@users.noreply.github.com>
2017-08-08 16:03:44 -0500

    add mdns

--- GarageServer.ino
+++ GarageServer.ino
@@ -1,36 +1,59 @@
 
 #include <GarageServer.h>
-#include <WiFiUdp.h>
+#include <OTAService.h>
+#include <ESP8266mDNS.h>
 
 //global variables
-String BSSID = "Garage";
+String BSSID = "esp_garage";
+String OTA_PASSWORD = "ronan";
 String Networks[][2] = {
 };
-
-
 GarageServer *server;
 void OnEvent(System_Event_t*se);
 
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("");
-  InitServer();
+  Serial.println("Booting");
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  for(int i=0;i<10;i++){
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+  }
+  Serial.println("Reading Chip Id");
+  long chipId = ESP.getChipId();
+  BSSID = BSSID + "_" + chipId;
   
+  Serial.print("Chip Id: ");
+  Serial.println(chipId);
+  Serial.println("Connecting to network");
+  InitServer();
+  OTAService.begin(8266,BSSID,OTA_PASSWORD);
+  
+  //setup mdns responder
+  if (!MDNS.begin(BSSID.c_str())) {
+    Serial.println("Error setting up MDNS responder!");
+  } else {
+	  //mdns to let devices know we have a http server
+	  MDNS.addService("http", "tcp", 80);
+  }
+  
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
-  delay(10);
+  yield();
   server->HandleClient();
-  broadcast();
-  multicast();
+  OTAService.handle();
 }
 
 void InitServer() {
   Serial.println("InitServer()");
 
   server =  new GarageServer();
-  server->Initialize(D1,D4);
+  server->Initialize(D1,D2);
   
   //register callback events
   Serial.println("registering callback events");
@@ -56,13 +79,12 @@ void InitServer() {
     if(!connected) {
       Serial.println("Failed to connect to " + ssid);
       if(++i >= iMax) {
-        i=0;
+        ESP.restart();
       }
     }
   }
   Serial.println("connecting to network complete");
   Serial.println("Connected to \"" + ssid + "\" with psk \"" + psk + "\"");
-
 
   Serial.println("InitServer() complete");
 }
@@ -87,64 +109,3 @@ void OnIpAssigned(IPAddress ip){
   Serial.println("Local IP Address: " + ipToString(ip));
 };
 
-//Check if header is present and correct
-/*
-bool is_authenticated(){
-  Serial.println("Enter is_authenticated");
-  if (server.hasHeader("Cookie")){
-    Serial.print("Found cookie: ");
-    String cookie = server.header("Cookie");
-    Serial.println(cookie);
-    if (cookie.indexOf("ESPSESSIONID=1") != -1) {
-      Serial.println("Authenticated Successful");
-      return true;
-    }
-  }
-  Serial.println("Authenticated Failed");
-  return false;
-}
-*/
-WiFiUDP udp;
-long lastBroadcastTime = 0;
-long broadcastDelay = 20000;
-void broadcast() {
-  long currentTime = millis();
-  if(lastBroadcastTime + broadcastDelay >= currentTime) {
-    return;
-  }
-  lastBroadcastTime = currentTime;
-  
-  Serial.println("Broadcasting Message");
-  IPAddress ipBroadCast(192, 168, 1, 255);
-  unsigned int udpRemotePort=2391;
-  unsigned int udplocalPort=2390;
-  char udpBuffer[96];
-  String message =  "{\"name\": \"Garage Server\",\"ip\": \"" + ipToString(server->LocalIP) + "\"}" ; 
-  
-  strcpy(udpBuffer, message.c_str()); 
-  udp.beginPacket(ipBroadCast, udpRemotePort);
-  udp.write(udpBuffer, sizeof(udpBuffer));
-  udp.endPacket();
-  Serial.println("Broadcast Complete");
-}
-
-long lastMulticastTime = 0;
-int multicastDelay = 10000;
-void multicast() {
-  long currentTime = millis();
-  if(lastMulticastTime + multicastDelay >= currentTime) {
-    return;
-  }
-  lastMulticastTime = currentTime;
-  
-  Serial.println("Sending Multicast Message");
-  IPAddress ipMulticast(224, 0, 0, 250);
-  unsigned int port = 8266;
-  String message =  "{\"device\": \"ESP8266\",\"name\": \"Garage Server\",\"ip\": \"" + ipToString(server->LocalIP) + "\"}" ; 
-  
-  udp.beginPacket(ipMulticast, port);
-  udp.write(message);
-  udp.endPacket();
-  
-  Serial.println("Multicast Complete");
-}
